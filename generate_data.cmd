0377c40720bd6c8b196f424e695ffcb1df78ad27
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-10-25 16:37:58 -0400

    initial upload to the repository

---
+++ generate_data.cmd
@@ -0,0 +1,7 @@
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_products_csv.python
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_products_json.python
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_products_parquet.python
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_quantity_csv.python
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_quantity_json.python
+%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe faker_quantity_parquet.python
+
